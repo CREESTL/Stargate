062ecb05dc113a59628da702126d23fc952e627e
IntegralTeam <sfxdx@sfxdx.com>
2022-08-16 17:46:54 +0300

    Add Comments to Bridge Token and Its Interface

--- contracts/BridgeTokenStandardERC20.sol
+++ contracts/BridgeTokenStandardERC20.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
 import "./interfaces/IBridgeTokenStandardERC20.sol";
 
+/// @title A custom ERC20 contract used in the bridge
 contract BridgeTokenStandardERC20 is IBridgeTokenStandardERC20, ERC20, Initializable {
 
     string internal __name;
@@ -15,13 +16,21 @@ contract BridgeTokenStandardERC20 is IBridgeTokenStandardERC20, ERC20, Initializ
 
     address public bridge;
 
+    ///@ dev The tokens does not need a name or symbol as it is used as an equivalent of other ERC20 tokens
     constructor() ERC20("", "") {}
 
+
+    /// @dev Checks if the caller is the bridge contract
     modifier onlyBridge {
-        require(_msgSender() == bridge, "onlyBridge");
+        require(_msgSender() == bridge, "BridgeToken: caller is not a bridge!");
         _;
     }
 
+    /// @notice Configures internal variables
+    /// @param _bridge The adress of the bridge for the tokens
+    /// @param _name The name of the token
+    /// @param _symbol The symbol of the token
+    /// @param _decimals The number of decimals of the token
     function configure(
         address _bridge,
         string memory _name,
@@ -34,23 +43,36 @@ contract BridgeTokenStandardERC20 is IBridgeTokenStandardERC20, ERC20, Initializ
         __decimals = _decimals;
     }
 
+    /// @notice Returns the name of the token
+    /// @return The name of the token
     function name() public view override(ERC20, IBridgeTokenStandardERC20) returns(string memory) {
         return __name;
     }
 
+    /// @notice Returns the symbol of the token
+    /// @return The symbol of the token
     function symbol() public view override(ERC20, IBridgeTokenStandardERC20) returns(string memory) {
         return __symbol;
     }
 
+    /// @notice Returns number of decimals of the token
+    /// @return The number of decimals of the token
     function decimals() public view override(ERC20, IBridgeTokenStandardERC20) returns(uint8) {
         return __decimals;
     }
 
+    /// @notice Creates tokens and assigns them to account, increasing the total supply.
+    /// @param _to The receiver of tokens
+    /// @param _amount The amount of tokens to mint
     function mint(address _to, uint256 _amount) public virtual override onlyBridge {
         _mint(_to, _amount);
         emit Mint(_to, _amount);
     }
 
+    
+    /// @notice Destroys tokens from account, reducing the total supply.
+    /// @param _from The address holding the tokens
+    /// @param _amount The amount of tokens to burn
     function burn(address _from, uint256 _amount) public virtual override onlyBridge {
         _burn(_from, _amount);
         emit Burn(_from, _amount);
--- contracts/interfaces/IBridgeTokenStandardERC20.sol
+++ contracts/interfaces/IBridgeTokenStandardERC20.sol
@@ -3,22 +3,51 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+
+/// @title An interface for a custom ERC20 contracts used in the bridge
 interface IBridgeTokenStandardERC20 is IERC20{
+    /// @notice Configures internal variables
+    /// @param _bridge The adress of the bridge for the tokens
+    /// @param _name The name of the token
+    /// @param _symbol The symbol of the token
+    /// @param _decimals The number of decimals of the token
     function configure(
         address _bridge,
         string memory _name,
         string memory _symbol,
         uint8 _decimals
     ) external;
-//    function bridgingToken() external returns (address);
+    
+    /// @notice Creates tokens and assigns them to account, increasing the total supply.
+    /// @param _to The receiver of tokens
+    /// @param _amount The amount of tokens to mint
     function mint(address _to, uint256 _amount) external;
+    
+    /// @notice Destroys tokens from account, reducing the total supply.
+    /// @param _from The address holding the tokens
+    /// @param _amount The amount of tokens to burn
     function burn(address _from, uint256 _amount) external;
 
+    /// @notice Returns the name of the token
+    /// @return The name of the token
     function name() external view returns(string memory);
+
+    /// @notice Returns the symbol of the token
+    /// @return The symbol of the token
     function symbol() external view returns(string memory);
+
+    /// @notice Returns number of decimals of the token
+    /// @return The number of decimals of the token
     function decimals() external view returns(uint8);
+
+    /// @notice Returns the name of the bridge
+    /// @return The name of the bridge
     function bridge() external view returns(address);
 
+
+    /// @notice Is emitted on every mint of the token
     event Mint(address indexed _account, uint256 _amount);
+    
+    /// @notice Is emitted on every burn of the token
     event Burn(address indexed _account, uint256 _amount);
 }
