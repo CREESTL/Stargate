431d7d6bdcf15c26c11728edf42759855b3ecea2
IntegralTeam <sfxdx@sfxdx.com>
2022-08-17 22:43:03 +0300

    Add Wallet Creation Script.

--- scripts/createWallet.js
+++ scripts/createWallet.js
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT 
+
 const { ethers, network } = require("hardhat");
 
 /**
  * Creates a single random wallet to be used for testnet 
  * NOTE Address and private key of the wallet will be displayed in the terminal. Copy them and save somewhere else!
- * NOTE Place this wallet's private key into '.env' BTTC_PRIVATE_KEY in order to deploy conctracts from its address
  */
 
 async function main() {
