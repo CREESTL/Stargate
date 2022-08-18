f09cae50fbc93ee0f97574f8cfb7e763f8da24a2
IntegralTeam <sfxdx@sfxdx.com>
2022-08-18 21:20:02 +0300

    Add Original Chain and Original Tokens to the Factory

--- contracts/WrappedERC20Factory.sol
+++ contracts/WrappedERC20Factory.sol
@@ -14,8 +14,17 @@ contract WrappedERC20Factory is IWrappedERC20Factory, AccessControl {
 
 
     /// @dev Map of addresses of tokens in the original and target chains
-    // TODO add original chain names here (struct)
-    mapping(address => address) internal originalTargetTokens;
+    mapping(address => address) internal originalToWrappedTokens;
+
+    /// @dev Struct holds the name of the original chain and the address of the original token
+    /// @dev Used to see what was the original chain of the wrapped token
+    struct TokenInfo {
+        string originalChain;
+        address originalAddress;
+    }
+
+    /// @dev Map of addresses of wrapped tokens and addresses of original tokens and original chains
+    mapping(address => TokenInfo) internal wrappedToOriginalTokens;
 
     /// @dev Role required to call functions of the factory
     bytes32 public constant BOT_MESSANGER_ROLE = keccak256("BOT_MESSANGER_ROLE");
@@ -37,7 +46,7 @@ contract WrappedERC20Factory is IWrappedERC20Factory, AccessControl {
     function checkTargetToken(address originalToken) public view onlyAdmin returns (bool){
         require (originalToken != address(0), "Factory: original token can not have a zero address!");
         // If there is no value for `originalToken` key then address(0) will be returned from the map
-        if (originalTargetTokens[originalToken] != address(0)) {
+        if (originalToWrappedTokens[originalToken] != address(0)) {
             return true;
         }
         return false;
@@ -45,6 +54,7 @@ contract WrappedERC20Factory is IWrappedERC20Factory, AccessControl {
 
     /// @notice Creates a new wrapped token on the target chain
     /// @dev Should be deployed on the target chain
+    /// @param originalChain The name of the original chain
     /// @param originalToken The address of the original token
     /// @param name The name of the new token
     /// @param symbol The symbol of the new token
@@ -52,6 +62,7 @@ contract WrappedERC20Factory is IWrappedERC20Factory, AccessControl {
     /// @param bridge The address of the bridge of tokens
     /// @return The address of a new token
     function createNewToken(
+        string memory originalChain,
         address originalToken,
         string memory name,
         string memory symbol,
@@ -60,9 +71,14 @@ contract WrappedERC20Factory is IWrappedERC20Factory, AccessControl {
     ) external returns (address) {
         // This will create a new token on the same chain the factory is deployed on (target chain)
         WrappedERC20 wrappedToken = new WrappedERC20(name, symbol, decimals, bridge);
-        originalTargetTokens[originalToken] = address(wrappedToken);
+        // Map the original token to the wrapped token 
+        originalToWrappedTokens[originalToken] = address(wrappedToken);
 
-        emit CreateNewToken(address(wrappedToken));
+        // And do the same backwards: map the wrapped token to the original token and original chain
+        TokenInfo memory wrappedTokenInfo = TokenInfo(originalChain, originalToken);
+        wrappedToOriginalTokens[address(wrappedToken)] = wrappedTokenInfo;
+
+        emit CreateNewToken(originalChain, originalToken, wrappedToken.name(), address(wrappedToken));
         
         return address(wrappedToken);
     }
--- contracts/interfaces/IWrappedERC20Factory.sol
+++ contracts/interfaces/IWrappedERC20Factory.sol
@@ -7,11 +7,14 @@ pragma solidity ^0.8.0;
 interface IWrappedERC20Factory {
 
     /// @notice Creates a new token to be used in the bridge
+    /// @param originalChain The name of the original chain
+    /// @param originalToken The address of the original token on the original chain
     /// @param name The name of the new token
     /// @param symbol The symbol of the new token
     /// @param decimals The number of decimals of the new token
     /// @return The address of a new token
     function createNewToken(
+        string memory originalChain,
         address originalToken,
         string memory name,
         string memory symbol,
@@ -20,5 +23,10 @@ interface IWrappedERC20Factory {
     ) external returns(address);
 
     /// @dev Event gets emmited each time a new token is created
-    event CreateNewToken(address indexed token);
+    event CreateNewToken(
+        string indexed originalChain,
+        address originalToken,
+        string  name, 
+        address indexed token
+    );
 }
