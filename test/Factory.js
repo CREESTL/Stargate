77770b223c30213878cf1717f95eb59fa3833bc1
IntegralTeam <sfxdx@sfxdx.com>
2022-08-21 19:09:04 +0300

    Reformat Factory Tests

--- test/Factory.js
+++ test/Factory.js
@@ -30,70 +30,67 @@ describe('Factory', () => {
 
   });
 
-  describe("Factory", function () {
-
-    it('Should create a new token', async() => {
-      await expect(factory.createNewToken("ETH", acc3.address, "Integral", "SFXDX", 18, bridge.address)).to.emit(factory, "CreateNewToken")
-        .withArgs("ETH", acc3.address, "Integral", anyValue); // anyValue is used because token address in unknown
-    });
-
-    it('Should fail to create two identical contracts', async() => {
-      // Create the token
-      await factory.createNewToken("ETH", acc3.address, "Integral", "SFXDX", 18, bridge.address);
-      // Try to create the same token
-      await expect(factory.createNewToken("ETH", acc3.address, "Integral", "SFXDX", 18, bridge.address)).
-        to.be.revertedWith("Factory: wrapped token already exists!");
-    });
-
-    it('Should fail to find original chain and token address for a fake token', async() => {
-      // Use an address of non-existing token
-      await expect(factory.getOriginalToken(acc3.address)).to.be.revertedWith("Factory: no original token found for a wrapped token!");
-
-    });
-
-    it('Should find original chain and token address', async() => {
-      // Deploy a new token
-      await factory.createNewToken("ETH", acc3.address, "Integral", "SFXDX", 18, bridge.address);
-      // Get this token's address
-      let tokenAddress = await factory.getWrappedAddress("Integral");
-      // Check original chain and address
-      let [originalChain, originalAddress] = await factory.getOriginalToken(tokenAddress);
-      expect(originalChain).to.equal("ETH");
-      expect(originalAddress).to.equal(acc3.address);
-    });
-
-    it('Should fail to find wrapped token if it was not created', async() => {
-      // Use an address of non-existing token
-      expect(await factory.checkTargetToken(acc3.address)).to.equal(false);
-    });
-
-    it('Should fail to find the address of a wrapped token with a fake name', async() => {
-      await expect(factory.getWrappedAddress("IAMNOTATOKEN")).
-        to.be.revertedWith("Factory: no wrapped token with this name!");
-    });
-
-    it('Should revert with invalid arguments', async() => {
-
-      await expect(factory.createNewToken("", acc3.address, "Integral", "SFXDX", 18, bridge.address)).
-        to.be.revertedWith("Factory: chain name is too short!");
-      await expect(factory.createNewToken("ETH", acc3.address, "", "SFXDX", 18, bridge.address)).
-        to.be.revertedWith("Factory: new token name is too short!");
-      await expect(factory.createNewToken("ETH", acc3.address, "Integral", "", 18, bridge.address)).
-        to.be.revertedWith("Factory: new token symbol is too short!");
-      await expect(factory.createNewToken("ETH", acc3.address, "Integral", "SFXDX", 0, bridge.address)).
-        to.be.revertedWith("Factory: invalid decimals!");
-      await expect(factory.createNewToken("ETH", acc3.address, "Integral", "SFXDX", 18, addressZero)).
-        to.be.revertedWith("Factory: bridge can not have a zero address!");
-
-      await expect(factory.checkTargetToken(addressZero)).
-        to.be.revertedWith("Factory: original token can not have a zero address!");  
-
-      await expect(factory.getOriginalToken(addressZero)).
-        to.be.revertedWith("Factory: wrapped token can not have a zero address!");
-
-      await expect(factory.getWrappedAddress("")).
-        to.be.revertedWith("Factory: token name is too short!");    
-
-    });
+  it('Should create a new token', async() => {
+    await expect(factory.createNewToken("ETH", acc3.address, "Integral", "SFXDX", 18, bridge.address)).to.emit(factory, "CreateNewToken")
+      .withArgs("ETH", acc3.address, "Integral", anyValue); // anyValue is used because token address in unknown
   });
+
+  it('Should fail to create two identical contracts', async() => {
+    // Create the token
+    await factory.createNewToken("ETH", acc3.address, "Integral", "SFXDX", 18, bridge.address);
+    // Try to create the same token
+    await expect(factory.createNewToken("ETH", acc3.address, "Integral", "SFXDX", 18, bridge.address)).
+      to.be.revertedWith("Factory: wrapped token already exists!");
+  });
+
+  it('Should fail to find original chain and token address for a fake token', async() => {
+    // Use an address of non-existing token
+    await expect(factory.getOriginalToken(acc3.address)).to.be.revertedWith("Factory: no original token found for a wrapped token!");
+
+  });
+
+  it('Should find original chain and token address', async() => {
+    // Deploy a new token
+    await factory.createNewToken("ETH", acc3.address, "Integral", "SFXDX", 18, bridge.address);
+    // Get this token's address
+    let tokenAddress = await factory.getWrappedAddress("Integral");
+    // Check original chain and address
+    let [originalChain, originalAddress] = await factory.getOriginalToken(tokenAddress);
+    expect(originalChain).to.equal("ETH");
+    expect(originalAddress).to.equal(acc3.address);
+  });
+
+  it('Should fail to find wrapped token if it was not created', async() => {
+    // Use an address of non-existing token
+    expect(await factory.checkTargetToken(acc3.address)).to.equal(false);
+  });
+
+  it('Should fail to find the address of a wrapped token with a fake name', async() => {
+    await expect(factory.getWrappedAddress("IAMNOTATOKEN")).
+      to.be.revertedWith("Factory: no wrapped token with this name!");
+  });
+
+  it('Should revert with invalid arguments', async() => {
+
+    await expect(factory.createNewToken("", acc3.address, "Integral", "SFXDX", 18, bridge.address)).
+      to.be.revertedWith("Factory: chain name is too short!");
+    await expect(factory.createNewToken("ETH", acc3.address, "", "SFXDX", 18, bridge.address)).
+      to.be.revertedWith("Factory: new token name is too short!");
+    await expect(factory.createNewToken("ETH", acc3.address, "Integral", "", 18, bridge.address)).
+      to.be.revertedWith("Factory: new token symbol is too short!");
+    await expect(factory.createNewToken("ETH", acc3.address, "Integral", "SFXDX", 0, bridge.address)).
+      to.be.revertedWith("Factory: invalid decimals!");
+    await expect(factory.createNewToken("ETH", acc3.address, "Integral", "SFXDX", 18, addressZero)).
+      to.be.revertedWith("Factory: bridge can not have a zero address!");
+
+    await expect(factory.checkTargetToken(addressZero)).
+      to.be.revertedWith("Factory: original token can not have a zero address!");  
+
+    await expect(factory.getOriginalToken(addressZero)).
+      to.be.revertedWith("Factory: wrapped token can not have a zero address!");
+
+    await expect(factory.getWrappedAddress("")).
+      to.be.revertedWith("Factory: token name is too short!");    
+
+    });
 });
